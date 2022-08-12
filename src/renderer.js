11e2093055c9db613691a32603409c1069c959c4
Mantas <mantas.peciulis17@gmail.com>
2022-08-12 11:34:28 +0300

    show monitor data

--- src/renderer.js
+++ src/renderer.js
@@ -1,70 +1,65 @@
 const {
-    SerialPort
-  } = require('serialport')
-  const {
-    ReadlineParser
-  } = require('@serialport/parser-readline')
-  const tableify = require('tableify')
-    const div = document.querySelector('#dropdown-port');
-  
-  
-  async function listSerialPorts() {
-    await SerialPort.list().then((ports, err) => {
-      ports.forEach((port) => {
-        div.innerHTML += ` <option value="${port.path}">${port.friendlyName}</option>`;
-      })
+  SerialPort
+} = require('serialport')
+const {
+  ReadlineParser
+} = require('@serialport/parser-readline')
+const tableify = require('tableify')
+const div = document.querySelector('#dropdown-port');
+
+async function listSerialPorts() {
+  await SerialPort.list().then((ports, err) => {
+    ports.forEach((port) => {
+      div.innerHTML += ` <option value="${port.path}">${port.friendlyName}</option>`;
     })
-  }
-  
-  function listPorts() {
-    div.innerHTML = "";
-    listSerialPorts();
-    setTimeout(listPorts, 2000);
-  }
-  
-  listSerialPorts()
-  
-  function getPort() {
-    return div.value;
-  }
-  
-  async function connectPort() {
-    document.querySelector('#connect').disabled = true;
-    div.disabled = true;
-    const port = new SerialPort({
-      path: getPort(),
-      baudRate: 9600
-    }) 
-    parserPort(port);
-    disconnectPort(port);
-  }
-  
-  async function parserPort(port)
-  {
-    let serialMonitor = new Map();
-    const parser = port.pipe(new ReadlineParser({
-      delimiter: '\r\n'
-    }))
-    parser.on('data', (data) => {
-      serialMonitor.set('data', data);
-      console.log(serialMonitor);
-    })
-    console.log(serialMonitor);
-    tableHTML = tableify(serialMonitor)
+  })
+}
+
+function listPorts() {
+  div.innerHTML = "";
+  listSerialPorts();
+  setTimeout(listPorts, 2000);
+}
+
+listSerialPorts()
+
+function getPort() {
+  return div.value;
+}
+
+async function connectPort() {
+  document.querySelector('#connect').disabled = true;
+  div.disabled = true;
+  const port = new SerialPort({
+    path: getPort(),
+    baudRate: 9600
+  })
+  parserPort(port);
+  disconnectPort(port);
+}
+
+async function parserPort(port) {
+  const parser = port.pipe(new ReadlineParser({
+    delimiter: '\r\n'
+  }))
+  parser.on('data', (data) => {
+    tableHTML = tableify(data)
     document.getElementById('data').innerHTML = tableHTML
-  
-  }
-  function disconnectPort(port)
-  {
-    var buttonDisconnect = false;
-    document.getElementById('disconnect').addEventListener('click', function () {
-      buttonDisconnect = true;
-      div.disabled = false;
-      tableHTML = tableify('')
-      document.getElementById('data').innerHTML = tableHTML
-      document.querySelector('#connect').disabled = false;
-      port.close(function (err) {
-        console.log('port closed', err);
-      });
+  })
+
+
+}
+
+function disconnectPort(port) {
+  var buttonDisconnect = false;
+  document.getElementById('disconnect').addEventListener('click', function () {
+    buttonDisconnect = true;
+    div.disabled = false;
+    tableHTML = tableify('')
+    document.getElementById('data').innerHTML = tableHTML
+    document.querySelector('#connect').disabled = false;
+    port.close(function (err) {
+      console.log('port closed', err);
     });
-  }
+  });
+}
